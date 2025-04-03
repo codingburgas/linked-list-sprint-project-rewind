d5d701ea613feca30848228ec66b32b0422f6c26
IIIvanov22 <iiivanov22@codingburgas.bg>
2025-04-03 13:31:03 +0300

    Add linked list management functions to corresponding .cpp file

--- rewind/listFunctions.cpp
+++ rewind/listFunctions.cpp
@@ -1,1 +1,95 @@
 #include "listFunctions.h"
+
+void append(EVENT** head, const EVENT& newEvent) {
+    EVENT* newNode = new EVENT(newEvent);
+    newNode->next = nullptr;
+
+    if (!*head) {
+        *head = newNode;
+        return;
+    }
+
+    EVENT* temp = *head;
+    while (temp->next) temp = temp->next;
+    temp->next = newNode;
+}
+
+void prepend(EVENT** head, const EVENT& newEvent) {
+    EVENT* newNode = new EVENT(newEvent);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void insertAt(EVENT** head, int index, const EVENT& newEvent) {
+    if (index == 0 || !*head) {
+        prepend(head, newEvent);
+        return;
+    }
+
+    EVENT* temp = *head;
+    for (int i = 0; temp->next && i < index - 1; i++) {
+        temp = temp->next;
+    }
+
+    EVENT* newNode = new EVENT(newEvent);
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void removeBack(EVENT** head) {
+    if (!*head) return;
+    if (!(*head)->next) {
+        delete* head;
+        *head = nullptr;
+        return;
+    }
+
+    EVENT* temp = *head;
+    while (temp->next->next) temp = temp->next;
+
+    delete temp->next;
+    temp->next = nullptr;
+}
+
+void removeFront(EVENT** head) {
+    if (!*head) return;
+    EVENT* temp = *head;
+    *head = (*head)->next;
+    delete temp;
+}
+
+void removeAt(EVENT** head, int index) {
+    if (!*head) return;
+    if (index == 0) {
+        removeFront(head);
+        return;
+    }
+
+    EVENT* temp = *head;
+    for (int i = 0; temp->next && i < index - 1; i++) {
+        temp = temp->next;
+    }
+
+    if (!temp->next) return;  // If index is out of bounds
+
+    EVENT* toDelete = temp->next;
+    temp->next = temp->next->next;
+    delete toDelete;
+}
+
+void printList(EVENT* head) {
+    if (!head) return;
+
+    //Work in progress;
+
+    printList(head->next);
+}
+
+void clearList(EVENT** head) {
+    while (*head) {
+        EVENT* temp = *head;
+        *head = (*head)->next;
+        delete temp;
+    }
+    *head = nullptr;
+}
--- rewind/listFunctions.h
+++ rewind/listFunctions.h
@@ -14,16 +14,16 @@ struct EVENT {
 	EVENT* next;
 };
 
-void append(EVENT*& head, const EVENT& newEvent);
+void append(EVENT** head, const EVENT& newEvent);
 
-void prepend(EVENT*& head, const EVENT& newEvent);
+void prepend(EVENT** head, const EVENT& newEvent);
 
-void insertAt(EVENT*& head, int index, const EVENT& newEvent);
+void insertAt(EVENT** head, int index, const EVENT& newEvent);
 
-void removeBack(EVENT*& head);
+void removeBack(EVENT** head);
 
-void removeFront(EVENT*& head);
+void removeFront(EVENT** head);
 
 void printList(EVENT* head);
 
-void clearList(EVENT*& head);
+void clearList(EVENT** head);
